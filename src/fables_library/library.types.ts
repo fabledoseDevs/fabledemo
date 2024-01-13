02c6dff461ca3da295db17580445c7d6c7cf4f81
Paweł Modzelewski <last.dunadan@gmail.com>
2024-01-14 00:04:44 +0100

    feature(fables-library): proposed fable structure and types.

---
+++ src/fables_library/library.types.ts
@@ -0,0 +1,38 @@
+export interface Info {
+  id: string;
+  title: string;
+  author: String[];
+  translation: String[];
+  pictures: String[];
+  thumb: string;
+  cover: string;
+  excerpt: string;
+  summary: string;
+}
+
+export enum LAYOUT_VARIANTS {
+  SINGLE_COLUMN_TEXT_BOTTOM,
+  SINGLE_COLUMN_TEXT_MIDDLE,
+  SINGLE_COLUMN_TEXT_TOP,
+  TWO_COLUMNS_TEXT_LEFT,
+  TWO_COLUMNS_TEXT_RIGHT,
+  DIAGONAL_TLBR,
+  DIAGONAL_TRBL
+}
+
+export interface Picture {
+  general: string;
+  mobile: string;
+}
+
+export interface Slide {
+  slideId: number;
+  layout: LAYOUT_VARIANTS;
+  paragraphs: String[];
+  picture: Picture;
+}
+
+export interface Story {
+  info: Info;
+  content: Slide[];
+}
