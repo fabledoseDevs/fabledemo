fa3bb1e1e5c547d0c3896c7f500712696429aba0
Paweł Modzelewski <last.dunadan@gmail.com>
2024-01-14 19:42:33 +0100

    feat(fables-library): first fairy tales for test purpouse (test data)

--- src/fables_library/library.types.ts
+++ src/fables_library/library.types.ts
@@ -2,7 +2,7 @@ export interface Info {
   id: string;
   title: string;
   author: String[];
-  translation: String[];
+  translation?: String[];
   pictures: String[];
   thumb: string;
   cover: string;
@@ -17,7 +17,15 @@ export enum LAYOUT_VARIANTS {
   TWO_COLUMNS_TEXT_LEFT,
   TWO_COLUMNS_TEXT_RIGHT,
   DIAGONAL_TLBR,
-  DIAGONAL_TRBL
+  DIAGONAL_TRBL,
+  WILDCARD,
+}
+
+export interface WildcardData {
+  top: number;
+  left: number;
+  width: number;
+  height?: number;
 }
 
 export interface Picture {
@@ -28,6 +36,7 @@ export interface Picture {
 export interface Slide {
   slideId: number;
   layout: LAYOUT_VARIANTS;
+  widcardsData?: WildcardData[]
   paragraphs: String[];
   picture: Picture;
 }
--- src/fables_library/pl/library.ts
+++ src/fables_library/pl/library.ts
@@ -6,7 +6,6 @@ export const GOLDILOCKS_AND_THREE_BEARS: Story = {
     id: '230100_GOLDILOCK_PL',
     title: 'Złotowłosa i trzy misie',
     author: ['Paweł Modzelewski'],
-    translation: ['Jan Kowalski'],
     pictures: ['Edyta Petrzak'],
     thumb: '/',
     cover: '/',
@@ -35,6 +34,162 @@ export const GOLDILOCKS_AND_THREE_BEARS: Story = {
         general: '/',
         mobile: '/',
       }
-    },      
+    },
+    {
+      slideId: 3,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        '"Ta jest zbyt gorąca." Powiedziała, zjadłszy łyżeczkę owsianki z miski taty misia.',
+        '"Ta jest zbyt zimna." Powiedziała, po zjedzeniu łyżki owsianki z miseczki mamy misia.',
+        '"Ta jest w sam raz!" Powiedziała po spróbowaniu owsianki z miseczki małego misia. A potem zjadła całą jego owsiankę.',
+        
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 4,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        'Gdy skończyła, poczuła się zmęczona. Zaczęła więc krzątać się po nieswoim domu w poszukiwaniu miejsca, aby odpocząć. W salonie znalazła trzy fotele: duży fotel, mniejszy fotel i całkiem malutki fotelik.',
+        '"Ten jest zbyt twardy." Powiedziała po wdrapaniu się na największy fotel, który należał do taty misia.',
+        '"Ten jest zbyt miękki." Powiedziała, usiadłszy na mniejszym fotelu, który należał do mamy misia.',
+        '"Ten jest w sam raz!" Powiedziała gdy rozsiadła się wygodnie w najmniejszym fotelu, który należał do małego misia. I wtem TRACH! Noga od fotelika ułamała się, a dziewczynka wylądowała na podłodze.'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 5,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        'Szybko jednak wstała. A ponieważ dalej czuła się zmęczona, weszła po schodach na piętro domu. Tam chciała znaleźć inne, wygodne miejsce do odpoczynku. Tak trafiła do sypialni, w której stały trzy, starannie pościelone łóżeczka. Jedno wielkie, drugie nieco mniejsze i trzecie najmniejsze.',
+        '"To jest zbyt twarde." Powiedziała po ułożeniu się w największym łóżku.',
+        '"To jest zbyt miękkie." Powiedziała, rozgrzebawszy pościel w mniejszym łóżku.',
+        '"To jest w sam raz!" Powiedziała po wejściu pod kołderkę w trzecim, najmniejszym łóżeczku. I tam zasnęła.'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 6,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        'Wtem misie wróciły ze swojego porannego spaceru. I od razu po wejściu do kuchni zorientowały się, że coś jest nie tak. Podeszły do stolika, na którym stała owsianka i zdumione zaczęły ze sobą rozmawiać.',
+        '"Ktoś jadł moją owsiankę." Powiedział mocnym głosem tata miś po zerknięciu do swojej miseczki.',
+        '"I moją także." Powiedziała zdziwiona mama miś.',
+        '"Błeee!" Zajęczał mały miś-maruda. "Moją owsiankę ktoś zjadł w całości!"'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 7,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        'Wciąż czujne i nieufne misie przeszły do saloniku, aby sprawdzić, czy intruz wciąż krząta się po domu. Z wielkim zdziwieniem zaczęły badać swoje foteliki.',
+        '"Ktoś siedział na moim fotelu." Mocnym głosem powiedział tata miś.',
+        '"I na moim także." Powiedziała zatroskana mama miś.',
+        '"Błeee!" Zajęczał mały miś-maruda. "W moim foteliku ktoś ułamał nogę!"'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 8,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      widcardsData: [
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25},
+        {top: 10, left: 10, width: 25}
+      ],
+      paragraphs: [
+        'Nie znalazłszy intruza na dole, misie weszły po schodach na piętro, do swojej sypialni. Z wielkim zaskoczeniem oglądały swoje rozścielone posłania.',
+        '"Ktoś spał w moim łóżeczku." Pełen złości wymamrotał tata miś.',
+        '"I w moim także." Powiedziała mama miś.',
+        '"Błeee!" Zajęczał mały miś-maruda. "W moim łóżeczku nadal ktoś śpi!"'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 9,
+      layout: LAYOUT_VARIANTS.DIAGONAL_TLBR,
+      paragraphs: [
+        'Wtem śpiąca złotowłosa zamrugała oczami. Przebudziła się i zobaczywszy trzy misie stojące nad sobą, wykrzyknęła z przerażenia. Potem zerwała się z łóżka i jednym susem wyskoczyła z okna prosto w zarośla rosnące pod oknem. Po czym pobiegła w las!',
+        'Gdy dziewczynka biegła przez las, serce ją zakuło. "Co ja najlepszego zrobiłam?" Pomyślała. "Zjadła nie swoją owsiankę. Połamałam fotelik. I nabałaganiłam w sypialni." Zatrzymała się w biegu. "Nie mogę tego tak zostawić. Muszę wszystko naprawić!" Ze smutkiem na twarzy wróciła do domu misiów i zapukała do drzwi. Otworzył tata miś i ze srogą miną zapytał.'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 10,
+      layout: LAYOUT_VARIANTS.TWO_COLUMNS_TEXT_RIGHT,
+      paragraphs: [
+        '"Cóż to za zachowanie młoda damo? Nie wiesz, że nie wolno wchodzić do cudzego domu? Nie wiesz, że nie wolno zjadać czyjegoś jedzenia? Ani psuć mebli lub bałaganić w sypialni?"',
+        '"Przepraszam tato misiu. Zachowałam się niegrzecznie. Ale chcę to wszystko naprawić jeśli mi dasz szansę."',
+        'Zza pleców taty misia wyjrzała mama miś o łagodnym sercu "No myślę, że po takich przeprosinach musimy dać ci szansę dziewczynko." Powiedziała mama miś. "Proszę, wejdź!".',
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
+    {
+      slideId: 11,
+      layout: LAYOUT_VARIANTS.SINGLE_COLUMN_TEXT_TOP,
+      paragraphs: [
+        'Złotowłosa dziewczynka spędziła cały dzień u misiów. Najpierw pomogła mamie przygotować nową porcję owsianki. Potem razem z tatą naprawiła popsuty fotelik. A na koniec razem z małym misiem zaścieliła wszystkie łóżeczka. Po wszystkim usiadła z misiami do kolacji i razem z nimi zjadła najlepszą owsiankę na świecie. A na deser było nawet ciasto.',
+        'Chociaż wszyscy wiedzą, że rozrabiać nie wypada, to nie ma takiego dziecka, które nie psoci od czasu do czasu. Jednak ważne jest, aby zawsze przeprosić i naprawić to, co się popsuło.'
+      ],
+      picture: {
+        general: '/',
+        mobile: '/',
+      }
+    },
   ],
 };
