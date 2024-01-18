2d9a9ca5b5349d83c1fed528684e09a35079c1fd
Maciej <55045855+MaciejMichalski92@users.noreply.github.com>
2024-01-18 18:59:49 +0100

    Feat/separator component (#4)
    * demo Separator component
    
    * Separator component propper version
    
    * Separator with storybook component, update main.ts storybook config
    
    * after CR fixes

---
+++ src/components/Separator/Separator.styled.ts
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+import type { SeparatorType } from './Separator.types';
+
+export const StyledSeparator = styled.div<{ type: SeparatorType }>(
+  ({ type }) => ({
+    width: 'fit-content',
+    margin: type == 'SMALL' ? '80px auto' : '120px auto',
+  }),
+);
