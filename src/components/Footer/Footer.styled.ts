c30f4a559f2836d45cbd86eba4c688ea65e67ad2
Maciej Michalski <maciejmichalski92@gmail.com>
2024-01-22 09:13:59 +0100

    after CR

--- src/components/Footer/Footer.styled.ts
+++ src/components/Footer/Footer.styled.ts
@@ -1,16 +1,16 @@
 import styled from 'styled-components';
 
-export const StyledFooter = styled.div(({ theme }) => ({
-  display: 'flex',
-  flexDirection: 'column',
-  justifyContent: 'center',
-  alignItems: 'center',
-  gap: '20px',
-  padding: '25px 0',
+export const StyledFooter = styled.div`
+  display: flex,
+  flexDirection: column,
+  justifyContent: center,
+  alignItems: center,
+  gap: 20px,
+  padding: 25px 0,
   backgroundColor: theme.palette.secondary,
 
-  [`@media ${theme.media.tablet}`]: {
-    padding: '45px 0',
-    gap: '40px',
+  @media ${({ theme }) => theme.media.tablet} {
+    padding: 45px 0,
+    gap: 40px,
   },
-}));
+`;
