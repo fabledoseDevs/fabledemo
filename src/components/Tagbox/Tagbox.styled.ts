0be29549cafa46028127ef3f9f4f7132826cdf06
Paweł Modzelewski <last.dunadan@gmail.com>
2024-03-26 10:34:56 +0100

    fix(various): Tagbox style update

--- src/components/Tagbox/Tagbox.styled.ts
+++ src/components/Tagbox/Tagbox.styled.ts
@@ -8,65 +8,6 @@ const flexboxCentered = css`
   justify-content: center;
 `;
 
-export const TagboxModal = styled.div`
-  position: fixed;
-  top: 0;
-  left: 0;
-  z-index: ${({ theme }) => theme.zIndex.high};
-  ${flexboxCentered};
-  width: 100dvw;
-  height: 100dvh;
-  overflow: hidden;
-  background: ${({ theme }) => theme.palette.storyPage.textbox60};
-`;
-
-export const ExitLayer = styled.button`
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: 100dvw;
-  height: 100dvh;
-  overflow: hidden;
-  background: none;
-  opacity: 0;
-`;
-
-export const ExitButton = styled.button`
-  position: absolute;
-  top: -45px;
-  right: 0;
-  z-index: ${({ theme }) => theme.zIndex.veryTop};
-  width: auto;
-  height: 50px;
-  bottom: 10px;
-  background: none;
-  border: 0;
-  color: ${({ theme }) => theme.palette.secondary};
-  cursor: pointer;
-
-  svg {
-    width: 30px;
-    height: 30px;
-    color: ${({ theme }) => theme.palette.secondary};
-  }
-`;
-
-export const TagboxContainer = styled.div`
-  position: relative;
-  width: auto;
-  max-width: 90dvw;
-  height: auto;
-  max-height: 80dvh;
-  padding: 24px;
-  border-radius: 10px;
-  background: antiquewhite;
-  ${flexboxCentered};
-
-  @media ${({ theme }) => theme.media.tablet} {
-    max-width: 70dvw;
-  }
-`;
-
 export const Headline = styled.div`
   text-align: center;
   margin-bottom: 24px;
