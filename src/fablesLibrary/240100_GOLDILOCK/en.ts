180312ead41e3c9cd9c42919a9a5d9f7a445082b
Paweł Modzelewski <last.dunadan@gmail.com>
2024-03-26 18:53:50 +0100

    feature(translations): translations

---
+++ src/fablesLibrary/240100_GOLDILOCK/en.ts
@@ -0,0 +1,150 @@
+import cover_sd from '@/fablesLibrary/240100_GOLDILOCK/images/cover_1280x720.webm';
+import cover_hd from '@/fablesLibrary/240100_GOLDILOCK/images/cover_1920x1080.webm';
+import img_1_sd from '@/fablesLibrary/240100_GOLDILOCK/images/img_1_1280x720.webm';
+import img_1_hd from '@/fablesLibrary/240100_GOLDILOCK/images/img_1_1920x1080.webm';
+import img_2_sd from '@/fablesLibrary/240100_GOLDILOCK/images/img_2_1280x720.webm';
+import img_2_hd from '@/fablesLibrary/240100_GOLDILOCK/images/img_2_1920x1080.webm';
+import img_3_sd from '@/fablesLibrary/240100_GOLDILOCK/images/img_3_1280x720.webm';
+import img_3_hd from '@/fablesLibrary/240100_GOLDILOCK/images/img_3_1920x1080.webm';
+import img_6_sd from '@/fablesLibrary/240100_GOLDILOCK/images/img_6_1280x720.webm';
+import img_6_hd from '@/fablesLibrary/240100_GOLDILOCK/images/img_6_1920x1080.webm';
+import img_7_sd from '@/fablesLibrary/240100_GOLDILOCK/images/img_7_1280x720.webm';
+import img_7_hd from '@/fablesLibrary/240100_GOLDILOCK/images/img_7_1920x1080.webm';
+import decorator from '@/fablesLibrary240100_GOLDILOCK/images/decorator.png';
+import thumb from '@/fablesLibrary240100_GOLDILOCK/images/thumb.png';
+
+import type { Story } from '../library.types';
+import { LAYOUT_VARIANTS, TAG_NAMES } from '../library.types';
+
+export const GOLDILOCKS_AND_THREE_BEARS: Story = {
+  info: {
+    id: '240100_GOLDILOCK',
+    title: 'Goldilocks And Three Bears',
+    author: ['Pawel Modzelewski'],
+    pictures: ['Edyta Petrzak'],
+    translation: [],
+    thumb: thumb.src,
+    decorator: decorator.src,
+    cover: {
+      description: 'Goldilocks And Three Bears',
+      picSizes: {
+        '1080': cover_hd,
+        '720': cover_sd,
+      },
+    },
+    summary:
+      "The golden-haired girl stumbles upon the bears' cabin and starts causing mischief there, learning a lesson about the importance of responsibility.",
+    extendedSummary:
+      'An engaging version of the classic fairy tale "Goldilocks and the Three Bears" prepared by our team. When the golden-haired girl stumbles upon the bears\' cabin and starts causing mischief there, she learns a lesson about the importance of responsibility. This charming adaptation reminds both young and old children that our actions have consequences and it is never too late to make amends.',
+    synopsis:
+      "The fairy tale tells about three teddy bears who lived in a cozy forest. One day, a golden-haired girl, known for her mischievous nature, broke into their house and caused a ruckus, eating their porridge, breaking their armchair and making a mess in their bedroom. When the bears returned, they realized what had happened and started looking for the intruder. The golden-haired girl, terrified, decided to repair the damage and apologize to the bears. She spent the whole day with them, helping them prepare the porridge, fixing the car seat, and tidying up the bedroom. The fairy tale has a happy ending when the girl learns a lesson about respect for other people's property and enjoys dinner and dessert together with the teddy bears. He emphasizes that although children sometimes do mischief, it is important to apologize and correct mistakes.",
+    summaryDecor: '/images/decorator.png',
+    mainTags: [TAG_NAMES.AGE_3, TAG_NAMES.CONTEMPORARY_FABLE],
+    detailedTags: [
+      TAG_NAMES.RESPONSIBILITY,
+      TAG_NAMES.PRIVACY,
+      TAG_NAMES.REPARATION,
+    ],
+  },
+  content: [
+    {
+      slideId: 1,
+      layout: LAYOUT_VARIANTS.TWO_COLUMNS_TEXT_LEFT,
+      paragraphs: [
+        'Na skraju przytulnego lasu, w drewnianym domu żyły sobie trzy misie. Tata miś, który był duży i silny, mama miś o łagodnym sercu, oraz mały misio co był marudny. W domu misiów było przytulnie. Były w nim trzy fotele oraz trzy, starannie pościelone łóżeczka. Misiowa rodzina codziennie rano jadała owsiankę przygotowywaną przez mamę. To była najlepsza owsianka na świecie — z miodem i jagodami. Jednak bardziej niż owsiankę misie uwielbiały spacery po lesie. Pewnego ranka postanowiły ruszyć na taki spacer wyjątkowo wcześnie. Wstały od stołu, od śniadania, nie skończywszy owsianki, i wyszły do lasu.\n',
+      ],
+      picture: {
+        description: 'Lorem',
+        picSizes: {
+          '1080': img_1_hd,
+          '720': img_1_sd,
+        },
+      },
+    },
+    {
+      slideId: 2,
+      layout: LAYOUT_VARIANTS.TWO_COLUMNS_TEXT_RIGHT,
+      paragraphs: [
+        'Tak się złożyło, że tego dnia nieopodal ich domu przechadzała się złotowłosa dziewczynka. Niesforne to było dziecko, znane z psotnego zachowania i niebaczenia na innych. Złotowłosa zobaczywszy otwarty dom, wślizgnęła się do środka bez zaproszenia.\n',
+      ],
+      picture: {
+        description: 'Lorem',
+        picSizes: {
+          '1080': img_2_hd,
+          '720': img_2_sd,
+        },
+      },
+    },
+    {
+      slideId: 3,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      wildcardData: [
+        { top: 5, left: 2.5, width: 30 },
+        { top: 30, left: 45, width: 35 },
+        { top: 45, left: 50, width: 35 },
+        { top: 60, left: 55, width: 40 },
+      ],
+      paragraphs: [
+        'Będąc w środku, od razu poczuła w powietrzu zapach owsianki. Usiadła przy stole kuchennym i zaczęła wyjadać owsiankę należącą do misiów.',
+        '"Ta jest zbyt gorąca." Powiedziała, zjadłszy łyżeczkę owsianki z miski taty misia.',
+        '"Ta jest zbyt zimna." Powiedziała, po zjedzeniu łyżki owsianki z miseczki mamy misia.',
+        '"Ta jest w sam raz!" Powiedziała po spróbowaniu owsianki z miseczki małego misia. A potem zjadła całą jego owsiankę.',
+      ],
+      picture: {
+        description: 'Lorem',
+        picSizes: {
+          '1080': img_3_hd,
+          '720': img_3_sd,
+        },
+      },
+    },
+
+    {
+      slideId: 6,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      wildcardData: [
+        { top: 65, left: 2.5, width: 30 },
+        { top: 10, left: 62, width: 35 },
+        { top: 25, left: 57, width: 35 },
+        { top: 40, left: 52, width: 40 },
+      ],
+      paragraphs: [
+        'Wtem misie wróciły ze swojego porannego spaceru. I od razu po wejściu do kuchni zorientowały się, że coś jest nie tak. Podeszły do stolika, na którym stała owsianka i zdumione zaczęły ze sobą rozmawiać.',
+        '"Ktoś jadł moją owsiankę." Powiedział mocnym głosem tata miś po zerknięciu do swojej miseczki.',
+        '"I moją także." Powiedziała zdziwiona mama miś.',
+        'Błeee!" Zajęczał mały miś-maruda. "Moją owsiankę ktoś zjadł w całości!',
+      ],
+      picture: {
+        description: 'Lorem',
+        picSizes: {
+          '1080': img_6_hd,
+          '720': img_6_sd,
+        },
+      },
+    },
+
+    {
+      slideId: 7,
+      layout: LAYOUT_VARIANTS.WILDCARD,
+      wildcardData: [
+        { top: 5, left: 65, width: 30 },
+        { top: 30, left: 60, width: 35 },
+        { top: 45, left: 57, width: 35 },
+        { top: 60, left: 54, width: 40 },
+      ],
+      paragraphs: [
+        'Wciąż czujne i nieufne misie przeszły do saloniku, aby sprawdzić, czy intruz wciąż krząta się po domu. Z wielkim zdziwieniem zaczęły badać swoje foteliki.',
+        '"Ktoś siedział na moim fotelu." Mocnym głosem powiedział tata miś.',
+        '"I na moim także." Powiedziała zatroskana mama miś.',
+        '"Błeee!" Zajęczał mały miś-maruda. "W moim foteliku ktoś ułamał nogę!"',
+      ],
+      picture: {
+        description: 'Lorem',
+        picSizes: {
+          '1080': img_7_hd,
+          '720': img_7_sd,
+        },
+      },
+    },
+  ],
+};
---
+++ src/translation/pl.ts
@@ -0,0 +1,147 @@
+import type { translations } from './types';
+
+export const data: translations = {
+  landingPage: {
+    jumbotron: {
+      tagline_p1: 'Gdzie baśnie',
+      tagline_p2: 'ożywają',
+      tagline_p3: 'do czytania.',
+      ctaButton: 'Dowiedz się więcej',
+    },
+    storySection: {
+      parentInfo: 'Dla Rodzica',
+      ctaRead: 'Czytaj bajkę',
+    },
+    projectSummary: {
+      mainText: [
+        'Jesteśmy grupą przyjaciół i profesjonalistów, których łączy wspólna pasja do baśni i opowiadań dla dzieci. Są wśród nas doradcy ds. dzieci, nauczyciele przedszkolni i żłobkowi, programiści i artyści. Wszyscy kierujemy się przekonaniem, że czytanie dziecku bajek jest istotną częścią jego rozwoju. Mając to na uwadze, rozpoczęliśmy pracę nad stworzeniem magicznego portalu wypełnionego animowanymi książkami do czytania dla dzieci.',
+        'Chcemy oferować klasyczne baśnie, które przetrwały próbę czasu, opowiedziane nie tylko słowem, ale też animowaną ilustracją. Każda historia została wybrana pod kątem głębi i znaczenia, mając na celu wydobycie tego, co w dziecięcej wyobraźni najlepsze. Możesz bezpiecznie zostawić nasze bajki dziecku do samodzielnego czytania. Lub czytać je z własną pociechą, na przykład, na dobranoc. Albo samemu zatopić się w nich i poczuć znowu dzieckiem. Opowieści te zostały zaprojektowane tak, aby urzekać i inspirować zarówno młodych, jak i starszych.',
+        'Już wkrótce uruchomimy kampanię crowdfundingową! Zachęcamy Cię do wsparcia tego projektu. Bez twojego zaangażowania takie projekty jak ten — skupione wokół idei innej niż pieniądze — mają ograniczone szanse na zaistnienie. Dzięki Twojemu wsparciu możemy stworzyć interaktywną platformę, która zanurzy dzieci w magicznym świecie opowieści. Pozostań na bieżąco z dalszymi aktualizacjami. Uruchomimy naszą kampanię wcześniej, niż myślisz!',
+      ],
+      supplementaryText: [
+        'Zapraszamy Cię do zaangażowania w tę podróż. Śledź nas na profilach społecznościowych. Pozostań w kontakcie i jako pierwszy dowiaduj się o postępie prac nad projektem. Razem możemy rozpalić iskrę wyobraźni w sercach dzieci i sprawić, że animowane baśnie do czytania będą inspirowały przyszłe pokolenia.',
+        'Dziękujemy za przeczytanie tej wiadomości i rozważenie wsparcia. Do usłyszenia wkrótce!',
+      ],
+    },
+    footerText: 'Napisz do nas',
+  },
+  cookieBanner: {
+    headline:
+      'Czy przyniosłeś ciasteczka, aby umilić sobie spotkanie z bajkami?',
+    paragraph:
+      'My przynieśliśmy swoje, ale te nie nadają się do jedzenia. Nasze ciasteczka to niewielkie pliki, które zapisujemy na Twoim urządzeniu, aby móc dostarczyć Ci usługę w najlepszej jakości. <br />Zapisujemy w nich informacje o ustawieniach aplikacji, jakie zostały przez ciebie wybrane. Jeżeli nie wyrażasz na to zgody, opuść tę stronę.  Jeśli chciałbyś dowiedzieć się więcej, skontaktuj się z nami.',
+    button: 'OK',
+  },
+  tags: {
+    tagboxInfo: {
+      headline: 'Motywy i rekomendacje dla utworu',
+      synopsis: 'Streszczenie.',
+    },
+    tagElements: {
+      age3: {
+        title: 'Od 3 lat',
+        description: 'Utwór przeznaczony jest dla dzieci od lat trzech.',
+      },
+      age5: {
+        title: 'Od 5 lat',
+        description: 'Utwór przeznaczony jest dla dzieci od lat pięciu.',
+      },
+      classicFairytale: {
+        title: 'Baśń Klasyczna',
+        description:
+          'Fantastyczna opowieść wywodząca się z tradycji folklorystycznych.',
+      },
+      contemporaryFairytale: {
+        title: 'Baśń Współczesna',
+        description:
+          'Fantastyczne opowiadanie napisane nie dawniej niż 70 lat temu lub współczesna adaptacja baśni klasycznej.',
+      },
+      responsibility: {
+        title: 'Odpowiedzialność',
+        description:
+          'Utwór porusza kwestię odpowiedzialności za innych lub za swoje czyny.',
+      },
+      carefulness: {
+        title: 'Ostrożność',
+        description: 'Utwór poucza o zachowaniu ostrożności.',
+      },
+      helpfulness: {
+        title: 'Pomocniczość',
+        description: 'Utwór opowiada o istotności niesienia pomocy innym.',
+      },
+      diligence: {
+        title: 'Pracowitość',
+        description:
+          'Utwór porusza temat pracowitości i wartości z niej płynących.',
+      },
+      privacy: {
+        title: 'Prywatność',
+        description:
+          'Utwór odnosi się do kwestii poszanowania prywatności lub własności prywatnej.',
+      },
+      family: {
+        title: 'Rodzina',
+        description:
+          'Utwór zawiera komentarze na temat wartości płynących z więzi rodzinnych.',
+      },
+      death: {
+        title: 'Śmierć',
+        description:
+          'Utwór porusza temat śmierci lub opowiada o losach osób, które ze śmiercią się stykają.',
+      },
+      toxicRelations: {
+        title: 'Toksyczna relacja',
+        description:
+          'Utwór zawiera przestrogi odnoście toksycznych osób lub relacji.',
+      },
+      moderation: {
+        title: 'Umiar',
+        description: 'Utwór porusza motywy umiarkowania i poucza o umiarze.',
+      },
+      cooperation: {
+        title: 'Współpraca',
+        description:
+          'Utwór porusza kwestię współpracy i korzyści z niej płynących.',
+      },
+      reparation: {
+        title: 'Zadośćuczynienie',
+        description:
+          'Utwór porusza motyw zadośćuczynienia za wyrzadzone krzywdy.',
+      },
+      errorMessage: {
+        title: 'Błąd',
+        description: 'Któryś z motywów nam umknął. Powiadom nas o błędzie!',
+      },
+    },
+  },
+  story: {
+    illustrator: 'Ilustrcje',
+    translator: 'Tłumaczenia',
+    parentInfo: 'Dla rodzica',
+    ctaRead: 'Czytaj bajkę',
+    return: 'Wyjdź',
+  },
+  settings: {
+    textThemeTitle: 'Wygląd tekstu',
+    default: 'Domyślne',
+    whiteText: 'Biały Tekst',
+    yellowText: 'Żółty Tekst',
+    blackText: 'Czarny Tekst',
+    whiteTextBoxStandard: 'Biały Tekst / Delikant Cień',
+    whiteTextBoxIntense: 'Biały Tekst / Intensywny Cień',
+    blackTextBoxStandard: 'Żółty Tekst / Delikant Cień',
+    blackTextBoxIntense: 'Żółty Tekst / Intensywny Cień',
+    fontSizeTitle: 'Wielkość czcionki',
+    previewText:
+      'Dawno temu, w odległej krainie, otoczonej mrocznymi lasami i malowniczymi górami, żyła piękna królewna o imieniu Śnieżka. Jej skóra była biała jak śnieg, a włosy czarne jak atrament. Jednak szczęśliwe życie Śnieżki było zagrożone przez kaprys jej złej macochy, która ponad wszystko, pragnęła być uznawaną za najpięknijeszą...',
+  },
+  modal: {
+    close: 'Zamknij',
+  },
+  leaveStoryModal: {
+    returnQuestionText:
+      'Czy chesz zakończyć czytanie i wrócić do strony głównej?',
+    yes: 'Tak, wracam do strony głównej',
+    no: 'Nie, chcę czytać dalej',
+  },
+};
