4e48a660ef985090f409a9502b26715643befc1a
Kuba Flis <jakub.flis@performance-media>
2024-03-29 10:19:41 +0100

    configure next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,11 @@
 // eslint-disable-next-line tsdoc/syntax
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  env: {
+    NEWSLETTER_SUBMISSION_KEY: process.env.NEWSLETTER_SUBMISSION_KEY,
+    NEWSLETTER_CAPTCHA_ID: process.env.NEWSLETTER_CAPTCHA_ID,
+  },
+
   webpack: config => {
     config.module.rules.push({
       test: /\.(webm)$/i,
