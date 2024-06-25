93cce4add65e32a1762aafbfb3134457d080fcdb
Paweł Modzelewski <last.dunadan@gmail.com>
2024-06-25 21:16:45 +0200

    fix(Modal): added exit on esc key.

--- src/components/ModalWindow/ModalWindow.tsx
+++ src/components/ModalWindow/ModalWindow.tsx
@@ -1,5 +1,5 @@
 import { Close } from '@styled-icons/ionicons-outline/Close';
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import {
   ExitButton,
@@ -9,15 +9,28 @@ import {
 } from './ModalWindow.styled';
 import type { ModalWindow as ModalWindowType } from './ModalWindow.types';
 
-export const ModalWindow: ModalWindowType = ({ exitFunction, children }) => (
-  <ModalWindowBody>
-    <ExitLayer onClick={() => exitFunction(false)} />
-    <ModalContentContainer>
-      <ExitButton onClick={() => exitFunction(false)}>
-        <span>Zamknij</span>
-        <Close />
-      </ExitButton>
-      {children}
-    </ModalContentContainer>
-  </ModalWindowBody>
-);
+export const ModalWindow: ModalWindowType = ({ exitFunction, children }) => {
+  useEffect(() => {
+    const handleEscKey = (event: KeyboardEvent) => {
+      event.key === 'Escape' && exitFunction(false);
+    };
+
+    document.addEventListener('keydown', handleEscKey);
+    return () => {
+      document.removeEventListener('keydown', handleEscKey);
+    };
+  }, [exitFunction]);
+
+  return (
+    <ModalWindowBody>
+      <ExitLayer onClick={() => exitFunction(false)} />
+      <ModalContentContainer>
+        <ExitButton onClick={() => exitFunction(false)}>
+          <span>Zamknij</span>
+          <Close />
+        </ExitButton>
+        {children}
+      </ModalContentContainer>
+    </ModalWindowBody>
+  );
+};
