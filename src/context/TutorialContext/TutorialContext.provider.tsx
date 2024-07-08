2bb148bc6015858affabaad38a3233b63e37d750
Paweł Modzelewski <last.dunadan@gmail.com>
2024-07-08 09:57:28 +0200

    feature(Tutorial): Context for tutorial.

---
+++ src/context/TutorialContext/TutorialContext.provider.tsx
@@ -0,0 +1,43 @@
+import type { ReactNode } from 'react';
+import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useCookies } from 'react-cookie';
+
+import type {
+  TutorialContext as TutorialContextType,
+  UseTutorialContext as UseTutorialContextType,
+} from './TutorialContext.types';
+
+const TutorialContext = createContext<TutorialContextType | undefined>(
+  undefined,
+);
+
+export const TutorialProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [cookies, setCookie] = useCookies(['tutorial']);
+  const currentTutorialState = cookies.tutorial;
+
+  const [tutorialOff, setTutorialOff] = useState<boolean>(
+    currentTutorialState || false,
+  );
+
+  useEffect(() => {
+    setCookie('tutorial', tutorialOff, { maxAge: 2592000 });
+  }, [tutorialOff]);
+
+  return (
+    <TutorialContext.Provider value={{ tutorialOff, setTutorialOff }}>
+      {children}
+    </TutorialContext.Provider>
+  );
+};
+
+export const useTutorialContext: UseTutorialContextType = () => {
+  const context = useContext(TutorialContext);
+  if (!context) {
+    throw new Error(
+      'useTutorialContext must be used within a SettingsProvider',
+    );
+  }
+  return context;
+};
